236f6accfd41b081d44407a95ca156791497ccec
Ponmanoj P <munch@Ponmanojs-MacBook-Air.local>
2026-01-07 14:10:09 +0530

    paruthi moota

--- PW2/backend/approval_requests/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class ApprovalRequestsConfig(AppConfig):
-    name = 'approval_requests'
--- PW2/backend/requirements_engine/apps.py
+++
@@ -1,5 +0,0 @@
-from django.apps import AppConfig
-
-
-class RequirementsEngineConfig(AppConfig):
-    name = 'requirements_engine'
